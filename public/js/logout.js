8303986675d3b9dfd8214b527b3bf6d41f0ea62d
RyanEbanks <ryankebanks@gmail.com>
2023-04-23 13:50:33 -0400

    Did more handlebar work

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,16 @@
+const logout = async () => {
+    // makes a request to this server enpoint
+    const response = await fetch('/api/user/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, 
+    });
+    //If the response is okay then the page is redirected to the homepage
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out.');
+    }
+};
 
+//When clicked the logout function above will be called
+document.querySelector('#logout').addEventListener('click', logout);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,23 @@
-
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({ user_name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
