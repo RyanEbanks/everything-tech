8753e9568ef5b3e2c6bd65941cdf3a67f391145c
RyanEbanks <ryankebanks@gmail.com>
2023-04-28 20:25:01 -0400

    Almost done, just need to get the dashboard route to work

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,45 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
+const { Post, Comment, User } = require('../models/');
 const withAuth = require('../utils/auth');
 
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        console.log('dashboard route hit'); // Add this line
+        const dashboardData = await Post.findAll({
+            include: [
+              {
+                model: User,
+              },
+              {
+                model: Comment,
+                include: [
+                  {
+                    model: User,
+                    attributes: ['email']
+                  }
+                ]
+              }
+            ],
+            where: {
+              email: req.session.user_email
+            }
+          });
+          
+        
+        const dashboard = dashboardData.map((post) => post.get({ plain: true }));
+    
+        if (!dashboard || dashboard.length === 0) {
+          res.status(404).json({ message: 'This user currently has no posts' });
+          return;
+        }
+    
+        res.render('dashboard', { dashboard, user_id: req.session.id, logged_in: req.session.logged_in });
+        console.log('\n\nreq.session being logged in', req.session.logged_in, '\n\n');
+        console.log('email:', req.session.user_email);
+      } catch(err) {
+        res.status(500).json(err);
+      }
+  });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,28 +1,32 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 
+
+
 // get all posts for homepage
 router.get('/', async (req, res) => {
-
   try {
     const postData = await Post.findAll({
       include: [{ all: true }],
     });
 
     const post = postData.map((post) => post.get({ plain: true }));
-    
+
     if (!post || post.length === 0) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-    
-    // res.status(200).json("Homepage Route Working!");
+
     res.render('all-posts', { post, id: req.session.id, logged_in: req.session.logged_in });
     console.log('\n\nreq.session being logged in', req.session.logged_in, '\n\n');
   } catch(err) {
     res.status(500).json(err);
   }
 });
+
+
+
+
 
 //Get Single Post
 router.get('/post/:id', async (req, res) => {
